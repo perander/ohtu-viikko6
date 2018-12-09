7a88e464e3d1110d025396f9b36e80975af2c9e3
perander <silva.perander@helsinki.fi>
2018-12-09 21:29:20 +0200

    refaktoroitu summa, erotus ja nollaa

---
+++ Laskin/src/main/java/laskin/Erotus.java
@@ -0,0 +1,50 @@
+package laskin;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.awt.*;
+
+public class Erotus implements Komento {
+    private TextField tuloskentta;
+    private TextField syotekentta;
+    private Button nollaa;
+    private Button undo;
+    private Sovelluslogiikka sovellus;
+
+
+    public Erotus (TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        this.tuloskentta = tuloskentta;
+        this.syotekentta = syotekentta;
+        this.nollaa = nollaa;
+        this.undo = undo;
+        this.sovellus = sovellus;
+    }
+
+    @Override
+    public void suorita() {
+        int arvo = 0;
+
+        try {
+            arvo = Integer.parseInt(syotekentta.getText());
+        } catch (Exception e) {
+        }
+
+        sovellus.miinus(arvo);
+
+        int laskunTulos = sovellus.tulos();
+
+        syotekentta.setText("");
+        tuloskentta.setText("" + laskunTulos);
+
+        if ( laskunTulos==0) {
+            nollaa.disableProperty().set(true);
+        } else {
+            nollaa.disableProperty().set(false);
+        }
+        undo.disableProperty().set(false);
+    }
+
+}
